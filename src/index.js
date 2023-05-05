1206827da7f4f4d767ae217609b11460a8299cf9
Helmut-Tischler <2010859027@fh-burgenland.at>
2023-05-05 15:42:52 +0200

    und wieder ein Test, Aufgeben NIEMALS

--- src/index.js
+++ src/index.js
@@ -15,6 +15,6 @@ root.render(
                 </Provider>} />
             </Routes>
         </HashRouter>
-    </React.StrictMode>
-
+    </React.StrictMode>,
+    document.getElementById("root")
 )
