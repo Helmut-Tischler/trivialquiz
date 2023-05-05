82ec7b6ea4d2b1c2036dee08490fe122f0a32367
Helmut-Tischler <2010859027@fh-burgenland.at>
2023-05-05 15:44:54 +0200

    Naja. Ein Versuch war es wert

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { HashRouter, Routes, Route } from 'react-router-dom/dist'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
-        <HashRouter>
-            <Routes>
-                <Route path="/" element={<Provider store={store}>
-                    <App />
-                </Provider>} />
-            </Routes>
-        </HashRouter>
-    </React.StrictMode>,
-    document.getElementById("root")
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
+
 )
